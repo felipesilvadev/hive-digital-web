8728caa3f58920861db12dfd8b3b2fca156f84e0
Felipe Silva <felipe.silva@assesi.com>
2025-03-23 19:07:12 -0300

    feat: sign in screen

--- src/pages/auth/sign-in.tsx
+++ src/pages/auth/sign-in.tsx
@@ -1,3 +1,51 @@
+import { useForm } from 'react-hook-form'
+import { toast } from 'sonner'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+
+const signInForm = z.object({
+  email: z.string().email(),
+})
+
+type SignInForm = z.infer<typeof signInForm>
+
 export function SignIn() {
-  return <h1>SignIn</h1>
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInForm>()
+
+  async function handleSignIn(data: SignInForm) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+    console.log(data)
+
+    toast.success('Enviamos um link de autenticação para seu e-mail')
+  }
+
+  return (
+    <div className="p-8">
+      <div className="flex w-[350px] flex-col justify-center gap-6">
+        <div className="flex flex-col gap-2 text-center">
+          <h1 className="text-2xl font-semibold tracking-tight">
+            Acessar dashboard
+          </h1>
+        </div>
+
+        <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">Seu e-mail</Label>
+            <Input id="email" type="email" {...register('email')} />
+          </div>
+
+          <Button className="w-full" type="submit" disabled={isSubmitting}>
+            Partiu
+          </Button>
+        </form>
+      </div>
+    </div>
+  )
 }
